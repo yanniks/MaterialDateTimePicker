1a41765a2fc6449ba0e2deb6475633e49e27e111
Yannik Ehlert <kontakt@yanniks.de>
2018-07-09 11:19:58 +0530

    Move to AndroidX

--- sample/src/main/java/com/wdullaer/datetimepickerexample/MainActivity.java
+++ sample/src/main/java/com/wdullaer/datetimepickerexample/MainActivity.java
@@ -1,13 +1,13 @@
 package com.wdullaer.datetimepickerexample;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
+import com.google.android.material.tabs.TabLayout;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.support.v13.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.legacy.app.FragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity
 {
